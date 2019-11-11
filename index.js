a4168a8ca865f25ae7bfdd293a509e6cc603e799
Krzysztof Podlaski <kpodlaski@gmail.com>
2019-11-11 22:52:22 +0100

    jfklsad

--- index.js
+++ index.js
@@ -6,28 +6,32 @@ const app = express();
 
 //initialize a simple http server
 const server = http.createServer(app);
-
-//initialize the WebSocket server instance
-
-const wss = new WebSocket.Server({ server });
-var wsClient;
-wss.on('connection', (ws) => {
-  openConnectionToFeatureProcessing();
-});
-
-//start our server
 server.listen(port, () => {
   console.log(`Server started on port ${server.address().port} :)`);
 });
-
+//initialize the WebSocket server instance
+var lastResult;
+const wss = new WebSocket.Server({ server });
+var wsClient;
+wss.on('connection', (ws) => {
+  if (lastResult) {
+    console.log('sending last result', lastResult.length);
+    ws.send(lastResult);
+  }
+  openConnectionToFeatureProcessing();
+});
 function openConnectionToFeatureProcessing() {
-  wsClient.close();
-  wsClient = new WebSocket(
-    'ws://webtask.future-processing.com:8068/ws/currencies',
-  );
-  wsClient.on('message', (data) => {
-    brodcastMessage(data);
-  });
+  console.log('open connection? ');
+  if (!wsClient) {
+    wsClient = new WebSocket(
+      'ws://webtask.future-processing.com:8068/ws/currencies',
+    );
+    wsClient.on('message', (data) => {
+      brodcastMessage(data);
+      lastResult = data;
+    });
+    console.log('create connection');
+  }
 }
 function brodcastMessage(data) {
   wss.clients.forEach((client) => client.send(data));
